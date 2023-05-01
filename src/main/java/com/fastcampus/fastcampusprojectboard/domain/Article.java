94ea3e6f9cdb4a2cdf087284c5fe5f0d16a1e6a1
henry1021 <fdgty5@naver.com>
2023-05-01 19:01:37 +0900

    auditor

--- src/main/java/com/fastcampus/fastcampusprojectboard/domain/Article.java
+++ src/main/java/com/fastcampus/fastcampusprojectboard/domain/Article.java
@@ -1,17 +1,42 @@
 package com.fastcampus.fastcampusprojectboard.domain;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import javax.persistence.*;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
-public class Article {
-    private Long id;
-    private String title;
-    private String hashtag;
+@Getter
+@ToString
+@Table(indexes = {
+        @Index(columnList = "title"),
+        @Index(columnList = "hashtag"),
+        @Index(columnList = "createdAt"),
+        @Index(columnList = "createdBy")
 
-    private LocalDateTime createdAt;
-    private String createdBy;
-    private LocalDateTime modifiedAt;
-    private String modifiedBy;
+})
+
+@Entity
+public class Article {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    private Long id;
+    @Setter private String title;
+    @Setter private String content;
+    @Setter private String hashtag;
+
+    @CreatedDate private LocalDateTime createdAt;
+    @CreatedBy private String createdBy;
+    @LastModifiedDate private LocalDateTime modifiedAt;
+    @LastModifiedBy private String modifiedBy;
 }
 
 
